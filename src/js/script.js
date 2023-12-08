31b345e1ee7cc6cf9cd0311175427e2f1ba55c26
Lucianoquintela <117698113+Lucianoquintela@users.noreply.github.com>
2023-12-08 17:19:10 +0000

    adicionado efeito no botão

--- src/js/script.js
+++ src/js/script.js
@@ -3,17 +3,25 @@ function exibirNota() {
 
     var notaParcial = document.getElementById('nota1').value
     var notaBimestral = document.getElementById('nota2').value
-    
+
     var notaParcialConvertida = parseInt(notaParcial)
     var notaBimestralConvertida= parseInt(notaBimestral)
-    
+
     calculoNotaGlobal = notaParcialConvertida + notaBimestralConvertida 
-    
+
     var resultado = calculoNotaGlobal / 2
     console.log(`Sua nota e: ${resultado}`)
 
     document.getElementById('exibirNota').innerHTML = resultado
 
-
+    if (resultado >= 6) {
+        console.log('Aprovado')
+        document.getElementById('exibirSituacao').innerHTML = "Aprovado"
+    } else if (resultado < 6) {
+        console.log('reprovado')
+        document.getElementById('exibirSituacao').innerHTML = "Reprovado"
+    } else {
+        console.log('Algo deu errado!')
+    }
 
 }
